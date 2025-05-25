400fd136f238cbc13992b779759312a4cb106e8a
TheExpertQA <ruchi.arora.om@gmail.com>
2025-05-25 12:57:12 +0100

    InvalidLogin Test

--- src/test/java/tests/InvalidLoginTest.java
+++ src/test/java/tests/InvalidLoginTest.java
@@ -1,35 +1,49 @@
 package tests;
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-    public class InvalidLoginTest {
-        WebDriver driver;
+import java.time.Duration;
 
-        @BeforeMethod
-        public void setUp() {
-            driver = new ChromeDriver();
-            driver.get("https://practicetestautomation.com/practice-test-login/");
-        }
+public class InvalidLoginTest {
+    WebDriver driver;
+    WebDriverWait wait;
 
-        @Test
-        public void testInvalidLogin() {
-            driver.findElement(By.id("username")).sendKeys("wronguser");
-            driver.findElement(By.id("password")).sendKeys("wrongpass");
-            driver.findElement(By.id("submit")).click();
+    @BeforeMethod
+    public void setUp() {
+        // Optional: Set path to chromedriver if not set globally
+        // System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
 
-            String errorMsg = driver.findElement(By.id("error")).getText();
-            Assert.assertTrue(errorMsg.contains("Your username is invalid!"));
-        }
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
 
-        @AfterMethod
-        public void tearDown() {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+    }
+
+    @Test
+    public void testInvalidLogin() {
+        driver.findElement(By.id("username")).sendKeys("wronguser");
+        driver.findElement(By.id("password")).sendKeys("wrongpass");
+        driver.findElement(By.id("submit")).click();
+
+        // Wait for error message
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("error")));
+        String errorMsg = driver.findElement(By.id("error")).getText();
+
+        Assert.assertEquals(errorMsg.trim(), "Your username is invalid!", "Error message mismatch.");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
             driver.quit();
         }
     }
-
-
+}
